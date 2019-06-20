2363c45ede6d6b1ff962ddaa33f58e8f77a89810
Nicolai Negru <negru.n@gmail.com>
2019-06-20 06:08:46 +0100

    Style 'who-is-coming' table

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,17 +2,17 @@ $(document).ready(whoIsComingTable);
 
 function whoIsComingTable() {
   $("#addrow").on("click", function() {
-    var newRow = $("<tr class='row'>");
+    var newRow = $("<tr class='row justify-content-center'>");
     var cols = "";
 
     cols +=
-      '<td class="col-lg-12 col-xl-5"><input type = "text" class="form-control" name = "name" placeholder = "Name" /></td >';
+      '<td class="col-lg col-xl-5"><input type="text" class="form-control" name="name" placeholder="Name" /></td >';
     cols +=
-      '<td class="col-lg"><input type="text" class="form-control" name="country" placeholder="Country"/></td>';
+      '<td class="col-lg"><input type="text" class="form-control" name="country" placeholder="Country" /></td>';
     cols +=
-      '<td class="col-lg"><input type="text" class="form-control" name="postcode" placeholder="Post Code"/></td>';
+      '<td class="col-lg"><input type="text" class="form-control" name="postcode" placeholder="Post Code" /></td>';
     cols +=
-      '<td class="col-1"><ion-icon name="trash" class="delete-participant badge-danger"/></td>';
+      '<td class="col-1 delete-participant-column block"><ion-icon name="trash" class="delete-participant badge-danger" /></td >';
     newRow.append(cols);
     $("table.who-is-coming").append(newRow);
   });
