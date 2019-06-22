3a72f49eb1d27d575dd52307155d055f8f79da08
Nicolai Negru <negru.n@gmail.com>
2019-06-22 12:48:03 +0100

    Dynamically add first row for participant

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,37 @@
-$(document).ready(whoIsComingTable);
+$(document).ready(setUp);
 
-function whoIsComingTable() {
-  $("#addrow").on("click", function() {
-    var newRow = $(
-      '<div class="participant-row row align-items-center justify-content-center">'
-    );
-    var cols = "";
+function setUp() {
+  addFirstRowToWhoIsComingTable();
+}
 
-    cols +=
-      '<input type="text" class="form-control col-lg col-xl-5" name="name" placeholder="Name" />';
-    cols +=
-      '<input type="text" class="form-control col-lg" name="country" placeholder="Country" />';
-    cols +=
-      '<input type="text" class="form-control col-lg" name="postcode" placeholder="Post Code" />';
-    cols +=
-      '<td class="col-1 delete-participant-column block"><ion-icon name="trash" class="delete-participant badge-danger" /></td >';
-    newRow.append(cols);
-    $("#who-is-coming-table").append(newRow);
-  });
+function addFirstRowToWhoIsComingTable() {
+  document.querySelector("#addrow").click();
+}
 
-  $("#who-is-coming-table").on("click", ".delete-participant-button", function(
-    event
-  ) {
-    $(this)
-      .closest("div")
-      .remove();
-  });
+function addParticipantRow() {
+  var newRow = $(
+    '<div class="participant-row row align-items-center justify-content-center">'
+  );
+
+  let cols =
+    '<input type="text" class="form-control col-lg col-xl-5" name="name" placeholder="Name" />';
+  cols +=
+    '<input type="text" class="form-control col-lg" name="country" placeholder="Country" />';
+  cols +=
+    '<input type="text" class="form-control col-lg" name="postcode" placeholder="Post Code" />';
+  cols += `<button type="button" class="btn btn-success confirm-participant-button" onclick="confirmParticipant(this)">
+        <ion-icon name="checkmark" class="confirm-participant-icon"></ion-icon></button>`;
+  cols += `<button type="button" class="btn btn-danger delete-participant-button" onclick="deleteParticipantRow(this)">
+            <ion-icon name="trash" class="delete-participant-icon" /></button>`;
+  newRow.append(cols);
+  $("#who-is-coming-table").append(newRow);
+  document.querySelector("#addrow").scrollIntoView(false);
+}
+
+function deleteParticipantRow(event) {
+  $(event)
+    .closest("div")
+    .remove();
 }
 
 const confirmParticipant = button => {
@@ -41,7 +47,7 @@ const confirmParticipant = button => {
 
 const fetchLocation = row => {
   console.log(`fetching location for ${row}... `);
-  return null;
+  return 1;
 };
 
 const notifyFetchLocationFailed = (row, participant) => {
